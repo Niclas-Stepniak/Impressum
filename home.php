4e67d284ff404ac50c9502cba68bcce4201a5377
Nic <niclas0409@googlemail.com>
2021-02-03 21:54:34 +0100

    First Files and Structures added
    Home.php added
    index.php
    readme.txt
    style.css
    a few Folders

---
+++ home.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This is my Homepage Template
+ *
+ */
+?>
+
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+  <title>Niclas Impressum</title>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<?php wp_head(); ?>
+</head>
+<body>
+  <div id="page">
+    <header>
+
+    </header>
+    <main></main>
+    <footer></footer>
+  </div><!-- #page -->
+</body>
+</html>
